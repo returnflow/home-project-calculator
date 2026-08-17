8aa75685ff1e24447b0e5b6fb05380a1a91b5095
Paul Alford <paul@returnflow.app>
2026-08-17 19:58:05 +0200

    fix: add sitemap-main.xml alternative and reference both in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: [`${baseUrl}/sitemap.xml`, `${baseUrl}/sitemap-main.xml`],
   }
 }
